4fe52843ca8cafc42acbd5866bd3b553943a46e6
username <wecp-dummy@gmail.com>
2025-09-29 09:42:33 +0000

    wecp initial commit

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -5,4 +5,53 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 //todo: complete missing code..
+   private token: string | null = null;
+    private isLoggedIn: boolean = false;
+  
+    constructor() {}
+  
+    saveToken(token: string) {
+      this.token = token;
+      this.isLoggedIn = true;
+      localStorage.setItem('token', token);
+    }
+     SetRole(role:any)
+    {
+      localStorage.setItem('role',role);
+    }
+    get getRole ():string|null
+    {
+      return localStorage.getItem('role');
+    }
+    SetUsername(username:any){
+      localStorage.setItem('username',username)
+    }
+  
+    get getUsername ():string|null{
+      return localStorage.getItem('username');
+    }
+    SetId(id:any)
+    {
+      localStorage.setItem('id',id);
+    }
+    get getId ():string|null
+    {
+      return localStorage.getItem('id');
+    }
+    
+    get getLoginStatus(): boolean {
+    
+        return !!localStorage.getItem('token');
+     
+    }
+    getToken(): string | null {
+     this.token= localStorage.getItem('token');
+      return this.token;
+    }
+    logout(){
+      localStorage.removeItem('token');
+      localStorage.removeItem('role');
+       this.token=null;
+       this.isLoggedIn=false
+     }
 }
--- server/src/main/java/com/wecp/logisticsmanagementandtrackingsystem/entity/Business.java
+++ server/src/main/java/com/wecp/logisticsmanagementandtrackingsystem/entity/Business.java
@@ -9,4 +9,5 @@ import java.util.List;
 
 public class Business {
     // implement entity here
+    
 }
